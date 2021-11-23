ac2fee3a2ffc287ffc63070c22911cd3909d1728
Yuriy Plyska <Yuriyply@gmail.com>
2021-11-23 21:58:36 +0200

    singelton added

---
+++ FactoryMethod/SingeltonPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SingeltonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProviderToDB provider = ProviderToDB.Instance;
+            provider.SqlQuery("select * from obj_Student with(nolock)");
+        }
+    }
+}
---
+++ FactoryMethod/SingeltonPattern/ProviderToDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingeltonPattern
+{
+    public class ProviderToDB
+    {
+        private static ProviderToDB provider = new ProviderToDB();
+        private ProviderToDB() { }
+
+        public static ProviderToDB Instance
+        {
+            get
+            {
+                return provider;
+            }
+        }
+
+        public void SqlQuery(string query)
+        {
+            Console.WriteLine(query);
+        }
+    }
+}
